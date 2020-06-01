0ab4c292f3bcee01b801e9e852471f0d8885b1ad
Zufar <zufar.adnan@gmail.com>
2020-06-01 13:29:57 +1000

    Added webpack configs

---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  modules: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+});
